d084e4a0f3d585f5faa8defdcf358f7a91697bc3
Gayasha Ranasinghe <gayasharanasinghe99@gmail.com>
2024-08-01 08:51:25 +0530

    Add files via upload

---
+++ Contacts/app.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+document.getElementById('sendButton').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the form from submitting
+  alert('Your message has been sent successfully!');
+});
